2a54a968c7a8c544a2f8a4c972f29a6e7607d2c9
nyingi <nyingimaina@gmail.com>
2026-02-24 09:32:30 +0300

    Changes

--- src/UI/DetailPane.tsx
+++ src/UI/DetailPane.tsx
@@ -22,7 +22,6 @@ export const DetailPane: React.FC<DetailPaneProps> = ({
   enableDesktopOverlay = true,
   onOverlayClick,
 }) => {
-  const flexBasis = desktopDetailPaneWidth || `calc(100% - ${sideWidth})`;
   const showOverlay = !isMobile && isSidePaneVisible && enableDesktopOverlay;
 
   return (
@@ -30,8 +29,8 @@ export const DetailPane: React.FC<DetailPaneProps> = ({
       className={styles.detailPane}
       style={{
         flex:
-          !isMobile && isSidePaneVisible
-            ? `0 0 ${flexBasis}`
+          !isMobile && isSidePaneVisible && desktopDetailPaneWidth
+            ? `0 0 ${desktopDetailPaneWidth}`
             : "1 1 100%",
         opacity: hydrated && !isMobile ? 1 : undefined,
         transition: hydrated
